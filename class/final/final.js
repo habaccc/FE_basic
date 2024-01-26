5d2fd73107464caa05f39356e56df8e25f5e1727
habaccc <haba.cicici@gmail.com>
2024-01-26 23:57:56 +0900

    final - signup (js)

--- class/final/final.js
+++ class/final/final.js
@@ -23,42 +23,52 @@ const changeFocus3 = () => {
     let p3 = document.getElementById("p3").value
 
     if(p1.length === 3 && p2.length === 4 && p3.length === 4) {
-        document.getElementById("send").style = "background-color: #FFFFFF; color: #0068FF; cursor: pointer;"
-        document.getElementById("send").removeAttribute("disabled")
+        document.getElementById("token__btn").style = "background-color: #FFFFFF; color: #0068FF; border: 1px solid #d2d2d2; cursor: pointer;"
+        document.getElementById("token__btn").removeAttribute("disabled")
     }
 }
 
-let isStarted = false;
-
-let auth = () => {
-
-    if(isStarted === false) {
-        // 타이머가 작동중이 아닐때
-        isStarted = true // true로 바꿔주어 작동시킴
-        document.getElementById("send").disabled = false
-        document.getElementById("send").style = "color: #d2d2d2;"
-        const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0") // 랜덤 번호 생성
-        document.getElementById("authNumber").innerText = token
-        
-        let time = 180
+const getToken = () => {
+    const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0")
+    document.getElementById("token").innerText = token
     
-        let timer = setInterval(function(){
-    
-            if(time >= 0){
-                let min = Math.floor( time / 60 )
-                let sec = String(time % 60).padStart(2, "0")
-                document.getElementById("timer").innerText = (min + ":" + sec)
-                time = time - 1
-            } else { // 0미만이기때문에 위의 if부분을 통과하지 못함
-                document.getElementById("send").disabled = true
-                isStarted = false
-                clearInterval(timer) //clearInterval안에 들어있는 함수를 삭제시켜줌.
-            }
-    
-        },1000)
+    document.getElementById("token__btn").style = "background-color: #FFFFFF; cursor: default;"
+    document.getElementById("token__btn").setAttribute("disabled", "true")
+    document.getElementById("token__confirm__btn").style = "background-color: #0068FF; color: #ffffff; border: 1px solid #d2d2d2; cursor: pointer;"
+    getTokenTimer()
+}
 
-    } else {
-        // 타이머가 작동중일때
-    }
+let intervall;
+const getTokenTimer = () => {
+    let timer = 10
+    interval = setInterval(() => {
+        if(timer >= 0){
+            const min = Math.floor(timer / 60)
+            const sec = timer % 60
 
+            document.getElementById("timer").innerText = min + ":" + String(sec).padStart(2, "0")
+            timer -= 1
+        } else {
+            document.getElementById("token").innerText = "000000"
+            document.getElementById("token__btn").style = ""
+            document.getElementById("token__btn").setAttribute("disabled", "true")
+
+            document.getElementById("timer").innerText = "3:00"
+            document.getElementById("token__confirm__btn").style = ""
+            document.getElementById("token__confirm__btn").setAttribute("disabled", "true")
+
+            clearInterval(interval)
+        }
+    }, 1000)
+}
+
+const getTokenTimerConfirm = () => {
+    clearInterval(interval)
+    document.getElementById("token__confirm__btn").style = "background-color: #FFFFFF; cursor: default;"
+    document.getElementById("token__confirm__btn").setAttribute("disabled", "true")
+    document.getElementById("token__confirm__btn").innerText = "인증완료"
+    alert("인증이 완료되었습니다.")
+
+    document.getElementById("signup__btn").style = "background-color: #FFFFFF; color: #0068FF; border: 1px solid #0068FF ;cursor: pointer;"
+    document.getElementById("signup__button").removeAttribute("disabled")
 }
